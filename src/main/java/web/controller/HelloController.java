5f13a227eee98ef9934ffa9cbe2c88c584d9e0e3
isaAmr007 <118926915+isaAmr007@users.noreply.github.com>
2023-04-12 06:01:00 +0300

    Add files via upload

---
+++ src/main/java/web/controller/HelloController.java
@@ -0,0 +1,68 @@
+package web.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import web.model.User;
+import web.service.UserService;
+
+import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class HelloController {
+	private UserService userService;
+
+
+
+
+	@Autowired
+	public void setUserService(UserService userService) {
+		this.userService = userService;
+	}
+
+	@ModelAttribute("newUser")
+	public User getPerson(){
+		return new User();
+	}
+	@GetMapping("/people")
+	public String index(Model model){
+		model.addAttribute("people",userService.getAllUsers());
+		return "view/index";
+	}
+
+	@PostMapping("/people")
+	public String creat(@ModelAttribute("newUser")@Valid User user,
+						BindingResult bindingResult,Model model) {
+		if (bindingResult.hasErrors()){
+			model.addAttribute("people",userService.getAllUsers());
+			return "view/index";
+		}
+
+		return "redirect:/people";
+	}
+
+	@DeleteMapping("/people/{id}")
+	public String deletePerson(@PathVariable("id") int id){
+		userService.removeUserById(id);
+		return "redirect:/people";
+	}
+	@GetMapping("/people/{id}/edit")
+	public String edit (@ModelAttribute("id") int id,Model model){
+		model.addAttribute("user",userService.getUserById(id));
+		return "view/edit";
+	}
+
+	@PatchMapping("/people/{id}")
+	public String updatePerson(@ModelAttribute("user")@Valid User user, BindingResult bindingResult){
+		if (bindingResult.hasErrors()){
+			return "view/edit";
+		}
+		userService.updateUser(user);
+		return "redirect:/people";
+	}
+}
